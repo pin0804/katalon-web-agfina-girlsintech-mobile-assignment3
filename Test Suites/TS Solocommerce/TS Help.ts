d5b90aba81c8902e434c58aa9095669b8c15fa52
agfina <agfinaa@gmail.com>
2023-09-01 16:10:39 +0700

    add tc help search

---
+++ Test Suites/TS Solocommerce/TS Help.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Help</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>76024c93-242f-4372-b202-32987bb21642</testSuiteGuid>
+   <testCaseLink>
+      <guid>df00873c-d565-478d-9827-055466b78eef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Solodroid_E-CommerceApp Demo/TC Help/TC_help</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ded58e6b-dcfe-46fb-96d3-f058213d31d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Solodroid_E-CommerceApp Demo/TC Help/TC_help search</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>db5a4632-68b1-471c-b086-0a951463258c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
